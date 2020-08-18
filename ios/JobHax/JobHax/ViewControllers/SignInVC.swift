feda0fb669cb55bfe4fa7ed87bee771b4dbb2a21
Yinan <yinancode@gmail.com>
2020-08-17 20:52:48 -0700

    created ios project

---
+++ ios/JobHax/JobHax/ViewControllers/SignInVC.swift
@@ -0,0 +1,21 @@
+//
+//  SignInVC.swift
+//  JobHax
+//
+//  Created by Yinan Qiu on 8/17/20.
+//  Copyright © 2020 Yinan. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
